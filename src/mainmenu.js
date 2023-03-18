e2b767874c3763fd83b807a9001156b2ca46e27f
NotNerwah <126194239+NotNerwah@users.noreply.github.com>
2023-03-18 05:07:57 +0000

    Started coding frontend

---
+++ src/mainmenu.js
@@ -0,0 +1,21 @@
+function mainmenu() {
+    return (
+    <div className="mainmenu">
+        <h1>Warehouse Inventory Manager</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="Inventory.html">Inventory Manager</a>
+                </li>
+                <li>
+                    <a href="Orders.html">Order Manager</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    );
+}
+export default mainmenu;
