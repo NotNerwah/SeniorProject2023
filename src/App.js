953240dc06351ab82377202f2ee5be59735c2496
NotNerwah <126194239+NotNerwah@users.noreply.github.com>
2023-03-29 02:57:18 +0000

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React,{useState,useEffect} from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components';
 import Home from './pages';
@@ -10,7 +10,7 @@ function App() {
   // eslint-disable-next-line
   return (
     <div className="App">
-      <Router><Navbar/><Routes><Route exact path='/' element={<Home />}/></Routes></Router>
+      <Router><Navbar/><Routes><Route exact path='/' element={<Home />}/><Route exact path='/inventory' element={<Inventory/>}/></Routes></Router>
     </div>
   );
 }
--- src/pages/inventory.js
+++ src/pages/inventory.js
@@ -3,11 +3,12 @@ import React from 'react'
 
 function displayInventory(inventory){
     let tHeads = <tr><td key="hsku">SKU</td><td key="hname">NAME</td><td key="hcategory">CATEGORY</td><td key="hquantity">QUANTITY</td><td key="hprice">PRICE</td></tr>
-    let tBody = inventory.map((d,i)=> <tr key ={"row " + i}><td key= {d.id + " sku"}>{d.sku}</td>
-    <td key= {d.id + " name"}>{d.name}</td>
-    <td key= {d.id + " category"}>{d.category}</td>
-    <td key= {d.id + " quantity"}>{d.quantity}</td>
-    <td key= {d.id + " price"}>{d.price}</td></tr>);
+    let tBody = inventory.map((d,i)=> <tr key ={"row " + i}>
+    <td key={d.sku + " sku"}>{d.sku}</td>
+    <td key={d.sku + " name"}>{d.name}</td>
+    <td key={d.sku + " category"}>{d.category}</td>
+    <td key={d.sku + " quantity"}>{d.quantity}</td>
+    <td key={d.sku + " price"}>{d.price}</td></tr>);
 
     return [tHeads,tBody]
 }
@@ -22,11 +23,11 @@ const Inventory = () => {
     const [inventory, setInventory] = React.useState([])
     let [tableHeads, tableBody] = [];
     React.useEffect(()=>{
-        fetchData('/inventory').then(r=>setInventory(r))
+        fetchData('/inventory').then(r => setInventory(r))
     }, [] )
     if (inventory.length !== 0){
         [tableHeads,tableBody] = displayInventory(inventory)
     }
-    return (<div><h2>What's in the warehouse?</h2><table><thead>{tableHeads}</thead><tbody>{tableBody}</tbody></table></div>)
+    return (<div><h2>What's in the warehouse?</h2><body><table><thead>{tableHeads}</thead><tbody>{tableBody}</tbody></table></body></div>)
 }
 export default Inventory;
